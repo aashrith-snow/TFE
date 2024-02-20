9f81cf0e68e6c71bd6f3e73b6e98ad49e0f95117
aashrith-snow <159412148+aashrith-snow@users.noreply.github.com>
2024-02-20 17:50:45 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "${var.subscriptionId}"
-  client_id       = "${var.clientId}"
-  client_secret   = "${var.clientSecret}"
-  tenant_id       = "${var.tenantId}"
-}
