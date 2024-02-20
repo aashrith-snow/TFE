bc23a1b5c365788ab03822f9dd94396120081096
aashrith-snow <159412148+aashrith-snow@users.noreply.github.com>
2024-02-20 17:22:46 +0530

    Add files via upload

--- CSC-Azure-Secure-Linux-VM/template.tf
+++ CSC-Azure-Secure-Linux-VM/template.tf
@@ -1,33 +1,38 @@
-
 data "azurerm_virtual_network" "tfresource" {
   name                = "${var.network}"
   resource_group_name = "${var.networkResourceGroup}"
 }
+
 data "azurerm_subnet" "tfresource" {
   name                 = "${var.subnet}"
   resource_group_name  = "${var.networkResourceGroup}"
   virtual_network_name = data.azurerm_virtual_network.tfresource.name
 }
+
 locals {
   _isSSHKey = "${var.isPassword ? {} : { empty = true }}"
 }
+
 resource "azurerm_resource_group" "tfresource" {
   count = "${var.isNewResourceGroup ? 1 : 0}"
   name = "${var.newResourceGroup}"
   location = "${var.region}"
 }
+
 resource "azurerm_public_ip" "tfresource" {
   name                = "${var.vmName}-public-ip"
   resource_group_name = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
   location            = "${var.region}"
   allocation_method   = "Dynamic"
 }
+
 resource "azurerm_network_security_group" "security_group" {
   count               = "${var.isAdvancedNetwork ? 0 : 1}"
   name                = "${var.vmName}-security-group"
   location            = "${var.region}"
   resource_group_name = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
 }
+
 resource "azurerm_network_security_rule" "ssh_rule" {
   count                       = "${!var.isAdvancedNetwork && var.ssh ? 1 : 0}"
   name                        = "SSH"
@@ -42,6 +47,7 @@ resource "azurerm_network_security_rule" "ssh_rule" {
   resource_group_name         = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
   network_security_group_name = "${azurerm_network_security_group.security_group[0].name}"
 }
+
 resource "azurerm_network_security_rule" "http_rule" {
   count                       = "${!var.isAdvancedNetwork && var.http ? 1 : 0}"
   name                        = "HTTP"
@@ -56,6 +62,7 @@ resource "azurerm_network_security_rule" "http_rule" {
   resource_group_name         = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
   network_security_group_name = "${azurerm_network_security_group.security_group[0].name}"
 }
+
 resource "azurerm_network_security_rule" "https_rule" {
   count                       = "${!var.isAdvancedNetwork && var.https ? 1 : 0}"
   name                        = "HTTPS"
@@ -70,15 +77,18 @@ resource "azurerm_network_security_rule" "https_rule" {
   resource_group_name         = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
   network_security_group_name = "${azurerm_network_security_group.security_group[0].name}"
 }
+
 data "azurerm_network_security_group" "existing_security_group" {
   count               = "${var.isAdvancedNetwork ? 1 : 0}"
   name                = "${var.nsgName}"
   resource_group_name = "${var.nsgResourceGroup}"
 }
+
 resource "azurerm_network_interface" "tfresource" {
   name                = "${var.nic}"
   location            = "${var.region}"
   resource_group_name = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.tfresource.id
@@ -86,11 +96,13 @@ resource "azurerm_network_interface" "tfresource" {
     public_ip_address_id = azurerm_public_ip.tfresource.id
   }
 }
+
 resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
   network_interface_id		= "${azurerm_network_interface.tfresource.id}"
   network_security_group_id	= "${var.isAdvancedNetwork ? data.azurerm_network_security_group.existing_security_group[0].id : azurerm_network_security_group.security_group[0].id}"
   depends_on = [azurerm_network_interface.tfresource, data.azurerm_network_security_group.existing_security_group, azurerm_network_security_group.security_group]
 }
+
 resource "azurerm_virtual_machine" "tfresource" {
   name                = "${var.vmName}"
   resource_group_name = "${var.isNewResourceGroup ? azurerm_resource_group.tfresource[0].name : var.existingResourceGroup}"
@@ -99,6 +111,7 @@ resource "azurerm_virtual_machine" "tfresource" {
   network_interface_ids = [
     azurerm_network_interface.tfresource.id,
   ]
+
   os_profile {
     computer_name  = "${var.vmName}"
     admin_username = "${var.adminUserName}"
@@ -114,18 +127,21 @@ resource "azurerm_virtual_machine" "tfresource" {
       }
     }
   }
+
   storage_os_disk {
     name		= "${var.vmName}-os-disk"
     caching              = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   storage_image_reference {
     publisher = "${var.image_publisher}"
     offer     = "${var.image_offer}"
     sku       = "${var.image_sku}"
     version   = "${var.image_version}"
   }
+
   delete_os_disk_on_termination = "${var.deleteOSDiskOnTerm}"
   depends_on = [azurerm_network_interface_security_group_association.nic_nsg_association]
 }
